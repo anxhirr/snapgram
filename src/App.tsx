8df962a6ad8ea78abcd77b7e3ce50b17afdcd0a6
anxhirr <anxhirr@gmail.com>
2023-10-28 18:17:44 +0200

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import SignUpForm from './_auth/forms/SignUpForm'
+import SigninForm from './_auth/forms/SigninForm'
+import { Home } from './_root/pages'
+import './globals.css'
+import { Routes, Route } from 'react-router-dom'
+
 export default function App() {
   return (
-    <div>App</div>
+    <main className='flex h-screen'>
+      <Routes>
+        <Route path='/sign-in' element={<SigninForm />} />
+        <Route path='/sign-up' element={<SignUpForm />} />
+
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
