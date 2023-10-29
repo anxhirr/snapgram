53bedb7aa5a7088b363c30e63716d8db18b6dc2a
anxhirr <anxhirr@gmail.com>
2023-10-29 12:01:30 +0100

    bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import AuthLayout from './_auth/AuthLayout'
 import SignUpForm from './_auth/forms/SignUpForm'
 import SigninForm from './_auth/forms/SigninForm'
 import RootLayout from './_root/RootLayout'
-import { Home } from './_root/pages'
+import {
+  AllUsers,
+  CreatePost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdatePost,
+  UpdateProfile,
+} from './_root/pages'
 import './globals.css'
 import { Routes, Route } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
@@ -18,6 +28,14 @@ export default function App() {
 
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<UpdatePost />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route path='/profile/:id/*' element={<Profile />} />
+          <Route path='/update-profile/:id' element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
