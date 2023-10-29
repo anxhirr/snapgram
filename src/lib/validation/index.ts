4a445a840429c30fc3a9719b4185038251f72795
anxhirr <anxhirr@gmail.com>
2023-10-29 11:19:55 +0100

    sign in

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -14,3 +14,11 @@ export const SignUpValidation = z.object({
     message: 'Minimum 8 characters are allowed',
   }),
 })
+export const SigninValidation = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email',
+  }),
+  password: z.string().min(8, {
+    message: 'Minimum 8 characters are allowed',
+  }),
+})
